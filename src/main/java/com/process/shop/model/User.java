0fe39059108878ed645b725a9a8026dfb1fa09d1
Pavazk <yosoypav4@gmail.com>
2024-03-21 21:12:36 -0500

    Se crea conexión con base de datos

--- src/main/java/com/process/shop/model/User.java
+++ src/main/java/com/process/shop/model/User.java
@@ -1,17 +1,26 @@
 package com.process.shop.model;
 
+import com.process.shop.model.enunm.DocumentType;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
+@Builder
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String fullName;
-    private Date birthDay;
-    private String typeIdentification;
+    private LocalDate birthDay;
+    private DocumentType typeIdentification;
     private String identification;
     private String phone;
     private String email;
     private String password;
-    private String address;
+    //private List<Address> address;
 }
--- src/main/java/com/process/shop/service/UserServiceImpl.java
+++ src/main/java/com/process/shop/service/UserServiceImpl.java
@@ -1,27 +1,47 @@
 package com.process.shop.service;
 
 import com.process.shop.model.User;
+import com.process.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User createUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(User userUpdated, Long id) {
-        return null;
+        Optional<User> userBb = userRepository.findById(id);
+        if (userBb.isEmpty()) {
+            return null;//todo: validar excepcion
+        }
+        User userToUpdate = userBb.get();
+        userToUpdate.setFullName(userUpdated.getFullName());
+        userToUpdate.setPhone(userUpdated.getPhone());
+        return userRepository.save(userToUpdate);
     }
 
     @Override
     public User getUserById(Long id) {
-        return new User();
+        Optional<User> userBb = userRepository.findById(id);
+        if(userBb.isEmpty()){
+            return null;//todo: validar excepcion
+        }
+        return userBb.get();
     }
 
     @Override
     public List<User> findAllUsers() {
-        return null;
+        return (List<User>) userRepository.findAll();
     }
 }
